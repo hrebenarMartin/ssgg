26e65141c5662b90498274e63bbbe658e3edb929
hrebenarMartin <m.hrebenar365@gmail.com>
2018-11-27 22:07:36 +0100

    test commit githubDesktop

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home2');
 Route::get('/about', 'AboutController@index')->name('about');
 Route::get('/contact', 'ContactController@index')->name('contact');
